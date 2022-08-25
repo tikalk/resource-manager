71affe65d10ccff1745e22cbe2b83bc96e28469b
Boris Komraz <borisk@cynet.com>
2022-08-25 20:27:39 +0300

    handlerFactory

--- controllers/resourcemanager_controller.go
+++ controllers/resourcemanager_controller.go
@@ -19,15 +19,15 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"time"
-
-	"github.com/tikalk/resource-manager/api/v1alpha1"
-	"github.com/tikalk/resource-manager/controllers/handlers"
+	resourcemanagmentv1alpha1 "github.com/tikalk/resource-manager/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -54,92 +54,106 @@ type ResourceManagerReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
 
-var collection map[types.NamespacedName]FHandler
+var collection map[types.NamespacedName]chan struct{}
 
-type FHandler struct {
-	F    func(stop chan bool)
-	Stop chan bool
+//type FHandler struct {
+//	F    func(ctx context.Context,
+//		//stopper chan struct{},
+//		stop chan bool,
+//		action string,
+//		condition []resourcemanagmentv1alpha1.ExpiryCondition,
+//		selector metav1.LabelSelector,
+//		clientset *kubernetes.Clientset)
+//	Stop chan bool
+//}
+
+var clientset *kubernetes.Clientset
+
+func handlerFactory(ctx context.Context, resourceType string) func(
+	ctx context.Context,
+	stopper chan struct{},
+	action string,
+	condition []resourcemanagmentv1alpha1.ExpiryCondition,
+	selectorLabels metav1.LabelSelector,
+	clientset *kubernetes.Clientset) {
+
+	return func(ctx context.Context,
+		stopper chan struct{},
+		action string,
+		condition []resourcemanagmentv1alpha1.ExpiryCondition,
+		selector metav1.LabelSelector,
+		clientset *kubernetes.Clientset) {
+		l := log.FromContext(ctx)
+		switch resourceType {
+		case "namespace":
+			HandleNamespace(ctx, stopper, action, condition, selector, clientset)
+		default:
+			l.Error(errors.NewBadRequest("math: square root of negative number"), fmt.Sprintf("Unexpected resourceType %s", resourceType))
+		}
+		//select {
+		//case <-stopper:
+		//	return
+		//default:
+		//	time.Sleep(time.Second * 5)
+		//}
+	}
 }
 
 func (r *ResourceManagerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 
 	l := log.FromContext(ctx)
-	name := req.NamespacedName.String()
+	//name := req.NamespacedName.String()
+	l.Info(fmt.Sprintf("ResourceManager object %s reconciled. Reconciling...", req.NamespacedName))
 
 	// your logic here
-	resourceManagerObj := &v1alpha1.ResourceManager{}
-	err := r.Get(ctx, req.NamespacedName, resourceManagerObj)
+	resourceManager := &resourcemanagmentv1alpha1.ResourceManager{}
+	err := r.Get(ctx, req.NamespacedName, resourceManager)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			l.Info(fmt.Sprintf("ResourceManager object %s has Not Found!!! \n", req.NamespacedName))
-			collection[req.NamespacedName].Stop <- true
-
-			// delete the key from collection map
+			l.Info(fmt.Sprintf("ResourceManager object %s deleted. Removing...", req.NamespacedName))
+			close(collection[req.NamespacedName])
 			delete(collection, req.NamespacedName)
 			return ctrl.Result{}, nil
 		}
 
-		l.Error(err, fmt.Sprintf("Failed reconcile obj %s", name))
+		l.Error(err, fmt.Sprintf("Failed reconcile obj %s", req.NamespacedName))
 	}
 
-	fmt.Printf("found ResourceManager object: %s \n", resourceManagerObj.Name)
+	fmt.Printf("found ResourceManager object: %s \n", resourceManager.Name)
 
-	// config handler object
-	h := handlers.Obj{
-		Name: req.NamespacedName,
-		C:    r.Client,
-		Ctx:  ctx,
-		L:    l,
-		Spec: resourceManagerObj.Spec,
-	}
-
-	l.Info(fmt.Sprintf(
-		"\n"+
-			" ResourceType: %s \n"+
-			" selectorLables %s \n"+
-			" action: %s \n"+
-			" condition: %s \n"+
-			" type: %s \n",
-		h.Spec.Resources,
-		h.Spec.Selector.MatchLabels,
-		h.Spec.Action,
-		h.Spec.Condition[0].After,
-		h.Spec.Condition[0].Type))
+	//// config handler object
+	//h := handlers.Obj{
+	//	Name: req.NamespacedName,
+	//	C:    r.Client,
+	//	Ctx:  ctx,
+	//	L:    l,
+	//	Spec: resourceManagerObj.Spec,
+	//}
+	//
+	//l.Info(fmt.Sprintf(
+	//	"\n"+
+	//		" ResourceType: %s \n"+
+	//		" selectorLables %s \n"+
+	//		" action: %s \n"+
+	//		" condition: %s \n"+
+	//		" type: %s \n",
+	//	h.Spec.Resources,
+	//	h.Spec.Selector.MatchLabels,
+	//	h.Spec.Action,
+	//	h.Spec.Condition[0].After,
+	//	h.Spec.Condition[0].Type))
 
 	// check if resource exists in our collection, if so, delete
-	if _, ok := collection[h.Name]; ok {
-		l.Info(fmt.Sprintf("Stopping loop for %s\n", h.Name))
-		collection[h.Name].Stop <- true
-		// delete the key from collection map
-		delete(collection, h.Name)
+	if _, ok := collection[req.NamespacedName]; ok {
+		l.Info(fmt.Sprintf("ResourceManager object %s found. Do nothing...", req.NamespacedName))
+		return ctrl.Result{}, nil
 	}
 
-	switch h.Spec.Resources {
-	case "namespace":
-		// add the function and its stop-channel to collection
-		collection[h.Name] = FHandler{
-			F: func(stop chan bool) {
-				for {
-					select {
-					case <-stop:
-						l.Info(fmt.Sprintf("%s Got stop signal!\n", h.Name))
-						return
-					default:
-						h.HandleNamespaceObj()
-						time.Sleep(5 * time.Second)
-					}
-				}
-			},
-			Stop: make(chan bool),
-		}
+	collection[req.NamespacedName] = make(chan struct{})
+	handler := handlerFactory(ctx, resourceManager.Spec.Resources)
+	l.Info(fmt.Sprintf("ResourceManager object %s created. Starting handler...", req.NamespacedName))
+	go handler(ctx, collection[req.NamespacedName], resourceManager.Spec.Action, resourceManager.Spec.Condition, *resourceManager.Spec.Selector, clientset)
 
-		// export to new var
-		c := collection[h.Name]
-
-		// execute in a new thread
-
-		go c.F(c.Stop)
-	}
 	//
 	//deploy := &appsv1.DeploymentList{}
 	//err = r.Client.List(ctx, deploy, &client.ListOptions{})
@@ -152,8 +166,18 @@ func (r *ResourceManagerReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ResourceManagerReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	collection = make(map[types.NamespacedName]FHandler)
+	collection = make(map[types.NamespacedName]chan struct{})
+
+	cfg, err := config.GetConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err = kubernetes.NewForConfig(cfg)
+	if err != nil {
+		panic(err.Error())
+	}
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&v1alpha1.ResourceManager{}).
+		For(&resourcemanagmentv1alpha1.ResourceManager{}).
 		Complete(r)
 }
