d39cae0657cc07a11ad01f528d08f3382b618c0a
Amitk3293 <amitkarni3293@gmail.com>
2022-08-30 13:26:06 +0200

    work still in progress files added

--- controllers/handlers/types.go
+++ controllers/handlers/types.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
 	"context"
 	"github.com/go-logr/logr"
 	resourcemanagmentv1alpha1 "github.com/tikalk/resource-manager/api/v1alpha1"
@@ -15,5 +16,14 @@ type Obj struct {
 	C    client.Client
 	Ctx  context.Context
 	L    logr.Logger
+	Spec resourcemanagmentv1alpha1.ResourceManagerSpec
+}
+
+
+type depObj struct {
+	Name appsv1.Deployment
+	C client.Client
+	Ctx context.Context
+	L logr.Logger
 	Spec resourcemanagmentv1alpha1.ResourceManagerSpec
 }
