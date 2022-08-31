9f827ecfa9d6e43f2dafcb957347696f8732d303
Boris Komraz <borisk@cynet.com>
2022-08-31 14:32:53 +0300

    handlerFactory

--- controllers/handler_namespace.go
+++ controllers/handler_namespace.go
@@ -10,11 +10,35 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"time"
 )
 
 //+kubebuilder:rbac:groups=*,resources=namespaces,verbs=get;list;watch;update;patch;delete
 //+kubebuilder:rbac:groups=*,resources=namespaces/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=*,resources=namespaces/finalizers,verbs=update
+
+func actionHandler(ctx context.Context, stopper chan struct{}, namespace string,
+	clientset *kubernetes.Clientset) {
+	l := log.FromContext(ctx)
+	l.Info(fmt.Sprintf("actionHandler begin <%s>", namespace))
+
+out:
+	for {
+		select {
+		case <-stopper:
+			//panic(fmt.Errorf("HandleNamespace: %s", "channel is closed."))
+			//l.Info(fmt.Sprintf("HandleNamespace <%s>: action <%s>", action))
+			l.Info(fmt.Sprintf("actionHandler aborted <%s>", namespace))
+			break out
+		default:
+
+			l.Info(fmt.Sprintf("actionHandler action: <%s> ", namespace))
+			// Calling Sleep method
+			time.Sleep(5 * time.Second)
+		}
+	}
+	l.Info(fmt.Sprintf("actionHandler end <%s>", namespace))
+}
 
 func HandleNamespace(ctx context.Context,
 	stopper chan struct{},
@@ -25,7 +49,10 @@ func HandleNamespace(ctx context.Context,
 	clientset *kubernetes.Clientset) {
 
 	l := log.FromContext(ctx)
-	l.Info(fmt.Sprintf("HandleNamespace <%s> begin", managedResource.Kind))
+	l.Info(fmt.Sprintf("HandleNamespace begin <%s>", managedResource.Kind))
+
+	//collection := make(map[types.NamespacedName]chan struct{})
+	collection := make(map[string]chan struct{})
 
 	selector, _ := metav1.LabelSelectorAsSelector(managedResource.Selector)
 	//l.Info(fmt.Sprintf("HandleNamespace selector: <%s>", selector.String()))
@@ -35,15 +62,16 @@ func HandleNamespace(ctx context.Context,
 	})
 
 	factory := informers.NewSharedInformerFactoryWithOptions(clientset, 0, informers.WithNamespace(namespace), labelOptions)
-
 	informer := factory.Core().V1().Namespaces().Informer()
-
 	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			//fmt.Println("namespace add")
 			name := obj.(*v1.Namespace).Name
 			labels := obj.(*v1.Namespace).Labels
 			fmt.Printf("Namespace added: %s\nLabels - %v\n\n", name, labels)
+			stopper := make(chan struct{})
+			collection[name] = stopper
+			go actionHandler(ctx, stopper, name, clientset)
 		},
 
 		UpdateFunc: func(oldObj interface{}, obj interface{}) {
@@ -51,6 +79,11 @@ func HandleNamespace(ctx context.Context,
 			name := obj.(*v1.Namespace).Name
 			labels := obj.(*v1.Namespace).Labels
 			fmt.Printf("Namespace updated: %s\nLabels - %v\n\n", name, labels)
+			close(collection[name])
+			delete(collection, name)
+			stopper := make(chan struct{})
+			collection[name] = stopper
+			go actionHandler(ctx, stopper, name, clientset)
 		},
 
 		DeleteFunc: func(obj interface{}) {
@@ -58,10 +91,17 @@ func HandleNamespace(ctx context.Context,
 			name := obj.(*v1.Namespace).Name
 			labels := obj.(*v1.Namespace).Labels
 			fmt.Printf("Namespace deleted: %s\nLabels - %v\n\n", name, labels)
+			close(collection[name])
+			delete(collection, name)
 		},
 	})
 
 	informer.Run(stopper)
+
+	l.Info(fmt.Sprintf("HandleNamespace cleanup: action <%s>", action))
+	for _, stopper := range collection {
+		close(stopper)
+	}
 
 	l.Info(fmt.Sprintf("HandleNamespace end: action <%s>", action))
 
